b5a4863e60a6be9221e8d8f2e489d0b4e5e98eed
Joost Hubregtse <61843535+hubregtsejoost@users.noreply.github.com>
2024-09-19 11:29:35 +0200

    Added animation
    Added fade in animation with intersectionObserver

--- Joost/script.js
+++ Joost/script.js
@@ -1,3 +1,16 @@
-const observer = IntersectionObserver((entries) => {
-    
-}, {});
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.toggle("in-view")
+        }
+    });
+}, {
+    rootMargin: "0px",
+    treshold: [0, 0.1, 1]
+})
+
+const tags = document.querySelectorAll(".card")
+
+tags.forEach((tag) => {
+    observer.observe(tag)
+})
